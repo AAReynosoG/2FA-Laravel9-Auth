c3b1d5c064800f96c457377fad8eb9c0f55fbcb7
Alonso <abrahamreynoso64@gmail.com>
2025-04-17 15:58:40 -0600

    Ejemplo de Write-Ride en laravel 9 para mysql

--- config/database.php
+++ config/database.php
@@ -45,12 +45,24 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'sticky' => true,
+
+            'write' => [
+                'host' => env('DB_MASTER_HOST'),
+                'port' => env('DB_MASTER_PORT'),
+                'username' => env('DB_MASTER_USERNAME'),
+                'password' => env('DB_MASTER_PASSWORD'),
+            ],
+
+            'read' => [
+                'host' => env('DB_SLAVE_HOST'),
+                'port' => env('DB_SLAVE_PORT'),
+                'username' => env('DB_SLAVE_USERNAME'),
+                'password' => env('DB_SLAVE_PASSWORD'),
+            ],
+
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -60,6 +72,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CERT => env('MYSQL_ATTR_SSL_CERT'),
+                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
             ]) : [],
         ],
 
